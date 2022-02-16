631b2f94bc12a4f9764bc16eef5de57c405f59cf
pviniciusm <pvinics@gmail.com>
2022-02-16 10:15:43 -0300

    add router and cards/login pages

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-    <h1>Aula 01 - React</h1>
-  );
-}
-
-export default App;
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Card(props) {
+
+    return(
+        <div className="card">
+            <p>{props.texto}</p>
+            <h4>{props.titulo}</h4>
+        </div>
+    )
+}
+
+export { Card };
